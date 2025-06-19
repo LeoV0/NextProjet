10431c4e4c8c3908bce98a614c5d06c41bbbdf8b
leoV0 <leo.cigale@gmail.com>
2025-06-19 12:10:26 +0200

    Remplacement prénom

--- pages/index.js
+++ pages/index.js
@@ -21,11 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Hello i'm Léo !]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>Hello i'm Léo !</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
